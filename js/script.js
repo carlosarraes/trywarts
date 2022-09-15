127e32551a88807ad542a5c185dd0f47e5d92316
carraes <carraeshb@gmail.com>
2022-09-15 10:59:12 -0300

    Adicionado darkmode

--- js/script.js
+++ js/script.js
@@ -1,31 +1,18 @@
-const signUpBtn = document.getElementById('signup-btn');
-const emailInput = document.getElementById('email-input');
-const passwordInput = document.getElementById('password-input');
 const isAgreed = document.getElementById('agreement');
 const submitBtn = document.getElementById('submit-btn');
 const textArea = document.getElementById('textarea');
 const showCounter = document.getElementById('counter');
 const evaluationForm = document.getElementById('evaluation-form');
 const formData = document.getElementById('form-data');
+const imgForm = document.getElementById('trybewarts-forms-logo');
 let counter = 500;
-showCounter.innerText = counter;
+showCounter.innerText = `${counter} caracteres`;
 const getName = document.getElementById('input-name');
 const getLastName = document.getElementById('input-lastname');
 const getEmail = document.getElementById('input-email');
 const getHouse = document.getElementById('house');
 const getObservacao = document.getElementById('textarea');
-
-// prettier-ignore
-function checkId() {
-  if (
-    emailInput.value === 'tryber@teste.com'
-    && passwordInput.value === '123456'
-  ) {
-    alert('Olá, Tryber!');
-  } else {
-    alert('Email ou senha inválidos.');
-  }
-}
+const toggle = document.getElementById('toggle');
 
 function handleButton() {
   if (isAgreed.checked === true) {
@@ -37,7 +24,17 @@ function handleButton() {
 
 function handleChange() {
   counter = 500 - textArea.value.length;
-  showCounter.innerText = counter;
+  showCounter.innerText = `${counter} caracteres`;
+
+  if (counter <= 100 && !showCounter.classList.contains('text-red-500')) {
+    showCounter.classList.add('text-red-500');
+    showCounter.classList.add('dark:text-red-500');
+    showCounter.classList.toggle('font-bold');
+  } else if (counter > 100 && showCounter.classList.contains('text-red-500')) {
+    showCounter.classList.remove('text-red-500');
+    showCounter.classList.remove('dark:text-red-500');
+    showCounter.classList.toggle('font-bold');
+  }
 }
 
 function fillData(familia, listMaterias, rate) {
@@ -74,6 +71,7 @@ function handleSubmit(e) {
   e.preventDefault();
   evaluationForm.classList.toggle('hidden');
   formData.classList.toggle('hidden');
+  imgForm.classList.toggle('hidden');
   const data = getData();
   const getSpan = document.querySelectorAll('span');
   getSpan[0].innerText = data.name;
@@ -85,7 +83,11 @@ function handleSubmit(e) {
   getSpan[6].innerText = data.observacao;
 }
 
+function handleDark() {
+  document.documentElement.classList.toggle('dark');
+}
+
 textArea.addEventListener('keyup', handleChange);
 isAgreed.addEventListener('change', handleButton);
-signUpBtn.addEventListener('click', checkId);
 submitBtn.addEventListener('click', handleSubmit);
+toggle.addEventListener('change', handleDark);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  darkMode: 'class',
   content: ['./*.html', './js/script.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ghostwhite: '#f8F5F2',
+        softGreen: '#078080',
+        darkBrown: '#232323',
+        strongOrange: '#f45d48',
+      },
+    },
   },
   plugins: [],
 };
